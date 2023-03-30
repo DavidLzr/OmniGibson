6ad7228d0747b461c7dfbcad9511c47fae14a406
Wensi Ai <vinceai@outlook.com>
2023-03-30 11:00:00 -0700

    Add profiling scripts

---
+++ tests/benchmark.sh
@@ -0,0 +1,17 @@
+pip install tqdm
+python omnigibson/examples/profiling/benchmark.py
+# python omnigibson/examples/profiling/benchmark.py -r
+# python omnigibson/examples/profiling/benchmark.py -f
+# python omnigibson/examples/profiling/benchmark.py -p
+# python omnigibson/examples/profiling/benchmark.py -s
+# python omnigibson/examples/profiling/benchmark.py -r -f
+# python omnigibson/examples/profiling/benchmark.py -r -p
+# python omnigibson/examples/profiling/benchmark.py -r -s
+# python omnigibson/examples/profiling/benchmark.py -f -p
+# python omnigibson/examples/profiling/benchmark.py -f -s
+# python omnigibson/examples/profiling/benchmark.py -p -s
+# python omnigibson/examples/profiling/benchmark.py -r -f -p
+# python omnigibson/examples/profiling/benchmark.py -r -f -s
+# python omnigibson/examples/profiling/benchmark.py -r -p -s
+# python omnigibson/examples/profiling/benchmark.py -f -p -s
+# python omnigibson/examples/profiling/benchmark.py -r -f -p -s
